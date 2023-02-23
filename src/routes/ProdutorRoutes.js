63a9bb41d27d9c4b5717917d2dba7be57ff26fb5
gustavobetete <gustavobetete@hotmail.com>
2023-02-23 14:25:06 -0300

    Envio de dados a cesta

--- src/routes/ProdutorRoutes.js
+++ src/routes/ProdutorRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Produtor from '../telas/Produtor/index'
 import Home from '../telas/Home'
+import Cesta from '../telas/Cesta'
 
 const Stack = createNativeStackNavigator();
 
@@ -11,5 +12,6 @@ export default function ProdutorRoutes({ ComponentePrincipal = Home }){
       }}>
         <Stack.Screen name='HomeScreen' component={ComponentePrincipal}/>
         <Stack.Screen name='Produtor' component={Produtor}/>
+        <Stack.Screen name='Cesta' component={Cesta}/>
     </Stack.Navigator>
 }
